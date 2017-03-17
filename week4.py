d5c8b485adafc7500c90d0c8b946d2ad63e87402
41511905 <philip.madin@students.mq.edu.au>
2017-03-17 15:51:06 +1100

    Week 3

--- week4.py
+++ week4.py
@@ -5,7 +5,7 @@ from nltk.corpus import names
 m = names.words('male.txt')
 f = names.words('female.txt')
 import random
-random.seed(54321) # Set the random seed to allow replicability
+random.seed(1234) # Set the random seed to allow replicability
 names = ([(name,'male') for name in m] +
          [(name,'female') for name in f])
 random.shuffle(names)
@@ -18,7 +18,7 @@ def gender_features2(word):
 train_set2=[(gender_features2(n),g) for (n,g) in train_names]
 devtest_set2=[(gender_features2(n),g) for (n,g) in devtest_names]
 classifier2=nltk.NaiveBayesClassifier.train(train_set2)
-nltk.classify.accuracy(classifier2,devtest_set2)
+#print(nltk.classify.accuracy(classifier2,devtest_set2))
 #print(classifier2.most_informative_features())
 #print(classifier2.show_most_informative_features())
 def gender_features5(word):
@@ -26,10 +26,31 @@ def gender_features5(word):
             'suffix2': word[-2:],
             'suffix3': word[-3:],
             'suffix4': word[-4:],
-            'suffix5': word[-5:]}
+            'suffix5': word[0],
+            'suffix6': word[:1]}
 
 train_set3=[(gender_features5(n),g) for (n,g) in train_names]
 devtest_set3=[(gender_features5(n),g) for (n,g) in devtest_names]
 classifier3=nltk.NaiveBayesClassifier.train(train_set3)
-nltk.classify.accuracy(classifier3,devtest_set3)
+test_set3=[(gender_features5(n),g) for (n,g) in test_names]
+classifier3=nltk.NaiveBayesClassifier.train(train_set3)
+print(nltk.classify.accuracy(classifier3,test_set3))
+#print(nltk.classify.accuracy(classifier3,devtest_set3))
 #print(classifier3.show_most_informative_features())
+train_accuracy2 = []
+devtest_accuracy2 = []
+nsamples = range(10,500,5)
+for n in nsamples:
+    classifier2 = nltk.NaiveBayesClassifier.train(train_set2[:n])
+    train_accuracy2.append(nltk.classify.accuracy(classifier2,train_set2[:n]))
+    devtest_accuracy2.append(nltk.classify.accuracy(classifier2,devtest_set2))
+
+from matplotlib import pyplot as plt
+plt.plot(nsamples,train_accuracy2,label='Train')
+plt.plot(nsamples,devtest_accuracy2,label='Devtest')
+plt.xlabel('Training size')
+plt.ylabel('Accuracy')
+plt.title('Classifier 2')
+
+plt.legend()
+plt.show()
